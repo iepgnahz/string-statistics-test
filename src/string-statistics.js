74bd8a5c8d0ade662a4732cb9bdda82458f632a1
zhangpei <13488233237@163.com>
2017-03-08 20:14:47 +0800

    add answer

--- src/string-statistics.js
+++ src/string-statistics.js
@@ -1,5 +1,41 @@
 function stringStatistics(string) {
   //在这里写入代码
+  const statistics = { letter: 0, number: 0, space: 0, special_characters: 0 };
+  const characters = string.split('');
+
+  characters.forEach(function(char) {
+    _parseCharacter(char, statistics);
+  });
+
+  return statistics;
+}
+
+function _parseCharacter(char, statistics) {
+  if (_isLetter(char)) {
+    statistics.letter ++;
+    return
+  }
+  if (_isNumber(char)) {
+    statistics.number ++;
+    return
+  }
+  if (_isSpace(char)) {
+    statistics.space ++;
+    return
+  }
+  statistics.special_characters ++;
+}
+
+function _isLetter(char) {
+  return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z');
+}
+
+function _isNumber(char) {
+  return char >= '0' && char <= '9';
+}
+
+function _isSpace(char) {
+  return char === ' ';
 }
 
 module.exports = stringStatistics;
